eef006c6c05d126cb277f9bde79ae30f61c8798a
Panav19 <panavkumar.sahu@gmail.com>
2026-05-23 00:50:50 +0530

    Fixed reset API for persistent ChromaDB architecture

--- backend/api/routes/reset_routes.py
+++ backend/api/routes/reset_routes.py
@@ -2,25 +2,26 @@ from fastapi import APIRouter
 import shutil
 import os
 
+from database.mongo import chat_collection
+
 router = APIRouter()
 
 @router.delete("/reset-knowledge-base")
 async def reset_knowledge_base():
 
     uploads_path = "uploads"
-    vector_db_path = "vector_db"
 
-    # Delete uploads
+    # DELETE PDF FILES ONLY
+
     if os.path.exists(uploads_path):
+
         shutil.rmtree(uploads_path)
 
-    # Delete vector database
-    if os.path.exists(vector_db_path):
-        shutil.rmtree(vector_db_path)
-
-    # Recreate folders
     os.makedirs(uploads_path, exist_ok=True)
-    os.makedirs(vector_db_path, exist_ok=True)
+
+    # CLEAR CHAT HISTORY
+
+    chat_collection.delete_many({})
 
     return {
         "message": "Knowledge base reset successfully"
