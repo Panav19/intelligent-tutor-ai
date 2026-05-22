a97197a18fc7ec3ee2355886bd79a0bd555cf612
Panav19 <panavkumar.sahu@gmail.com>
2026-05-22 12:50:56 +0530

    Configured FastAPI CORS for frontend integration

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from api.routes.upload_routes import router as upload_router
 from api.routes.qa_routes import router as qa_router
 
 app = FastAPI()
+
+# CORS CONFIGURATION
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(upload_router)
 app.include_router(qa_router)
